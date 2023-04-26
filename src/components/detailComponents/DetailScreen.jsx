57733c7b6fd2d967790e29758846e3779f4f34fd
Derek Wheeler <frozenarrow73@gmail.com>
2023-04-26 10:34:31 -0600

    added styling to details page

--- src/components/detailComponents/DetailScreen.jsx
+++ src/components/detailComponents/DetailScreen.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import salmon from '../../assets/salmon.jpg'
 
 const DetailScreen = () => {  
   return (
     <section>
-      {/* Welcome to the details page! This page will be reusable. Follow instructions to know what to do here. */}
+      <div
+      style={{
+        background: `linear-gradient(
+          190deg,
+          rgba(0, 0, 0, 0.8),
+          rgba(0, 0, 0, 0.8)),
+          url(${salmon})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        height: "300px",
+      }}
+    >
+      <div className="ad-content">
+        <h1>Pineapple Salmon</h1>
+      </div>
+    </div>
+    <div className='details-section'>
+      <div className='recipe-ingredients'>
+        <h2>Recipe</h2>
+        <p>example text</p>
+        <h2>Ingredents</h2>
+        <p>example text</p>
+      </div>
+      <div className='recipe-details'>
+        <h2>Details</h2>
+        <p>example text</p>
+      </div>
+    </div>
     </section>
   );
 };
